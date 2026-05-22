44af1cdac3bd02191d63c235a7e98ccef130cc8d
yourbottleyourbrandf <yourbottleyourbrand@gmail.com>
2026-05-22 22:42:49 +0500

    now images are also scraped

--- intermidiate-projects/project-2/books-scraper.py
+++ intermidiate-projects/project-2/books-scraper.py
@@ -1,10 +1,12 @@
 import csv
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 
 rows = []
 page_number = 1
+request_timeout = 30
 
 while True:
     if page_number == 1:
@@ -12,7 +14,7 @@ while True:
     else:
         url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=request_timeout)
     soup = BeautifulSoup(response.text, "html.parser")
     books = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
@@ -20,9 +22,11 @@ while True:
         break
 
     for book in books:
+        image = book.find("img")
         title = book.find("h3").find("a")["title"]
         price = book.find("p", class_="price_color").get_text(strip=True)
         availability = book.find("p", class_="instock availability").get_text(strip=True)
+        image_url = urljoin("https://books.toscrape.com/", image.get("src")) if image else ""
 
         rows.append(
             {
@@ -30,13 +34,14 @@ while True:
                 "Title": title,
                 "Price": price,
                 "Availability": availability,
+                "Image": image_url,
             }
         )
 
     page_number += 1
 
 with open("books.csv", "w", newline="", encoding="utf-8") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["Page", "Title", "Price", "Availability"])
+    writer = csv.DictWriter(csv_file, fieldnames=["Page", "Title", "Price", "Availability", "Image"])
     writer.writeheader()
     writer.writerows(rows)
 
